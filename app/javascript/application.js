9ad28e3122930180baeff419b17af5f3335c4f5a
Serzh <serzhkont@proton.me>
2025-08-25 20:11:40 +0300

    setup main documents page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,24 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Получаем все элементы с классом navbar-burger
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Проверяем, есть ли бургеры
+  if ($navbarBurgers.length > 0) {
+    // Добавляем обработчик клика для каждого бургера
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        // Получаем цель из data-target
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Переключаем класс is-active для бургера и меню
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
